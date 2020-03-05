5f6c86448060c4fc9a438981a1fffc08315abe17
bamba-sudo <bambasudo@yahoo.com>
2020-03-05 10:48:50 -0500

    change 10;47

---
+++ config.py
@@ -0,0 +1,4 @@
+# var - constante
+
+EMAIL_USER = 'monamiegoerge@gmail.com'
+EMAIL_SENDER = 'monamiegoerge@gmail.com'
--- script.py
+++ script.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 
 import smtplib
 
+from config import *
+
 '''
 email_user = 'monamiegoerge@gmail.com'
 email_send = 'monamiegoerge@gmail.com'
@@ -23,15 +25,18 @@ server.sendmail(email_user, email_send, message)
 server.quit()
 
 '''
+
 # Var
 path = r"C:\Users\Bamba\Documents\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(path)
 choice = 0
-link = "https://www.boursorama.com/cours/analyses/AAPL/"
+appleLink = "https://www.boursorama.com/cours/analyses/AAPL/"
 element = "c-faceplate__price " 
+alibabaLink = "https://www.boursorama.com/cours/BABA/"
 #
 
-# def
+
+# ============= def ======================
 def menu():
 	print("\n1. Apple"
 		"\n2. Alibaba"
@@ -45,12 +50,28 @@ def extracteur(unDriver, unLink, unElement):
 
 	for post in posts:
 		newPost = post.text[0:6]
-		# print("\n--> Apple : ", newPost," USD")
 
+	unDriver.quit()
 	return newPost
-def emailSender():
-	
-#
+
+def emailSender(unPost, unEmailUser, unEmailSender, unMessage):
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+
+	server.starttls()
+
+	server.login(unEmailUser, 'gangawix99')
+
+	message = unMessage+unPost
+
+	try:
+		server.sendmail(unEmailUser, unEmailSender, message)
+		print("Email sent")
+	except:
+		print("Error")
+	server.quit()
+
+		
+# ====================================== 
 
 # Debut du programme
 
@@ -64,39 +85,38 @@ while(choice != 1 and choice != 2 and choice != 3 and choice != 4):
 
 # Choix numero 1
 if(choice == 1):
-	print("\nVous avez choisi :", choice)
+	print("\nVous avez choisi : Apple")
 
 	# extraire le text du html pour le stocker dans une variable
 	# par la class="c-faceplate__price"
 	# tag <div></div>
 	
-	extracteur(driver, link, element)
+	post = extracteur(driver, appleLink, element)
 
-
-	'''
 	# va envoyer l'extrait du text pris du text par email
-	email_user = 'monamiegoerge@gmail.com'
+	emailSender(post, EMAIL_USER, EMAIL_SENDER, "Apple --> ")
 
-	email_send = 'monamiegoerge@gmail.com'
+# Choix numero 2
+if(choice == 2):
+	print("\nVous avez choisi : Alibaba")
 
-	server = smtplib.SMTP('smtp.gmail.com', 587)
+	post = extracteur(driver, alibabaLink, element)
 
-	server.starttls()
+	emailSender(post, EMAIL_USER, EMAIL_SENDER, "Alibaba --> ")
 
+if(choice == 3):
+	print("\nVous avez choisi: Facebook")
 
-	server.login(email_user, 'gangawix99')
+	post = extracteur(driver, alibabaLink, element)
 
+	emailSender(post, EMAIL_USER, EMAIL_SENDER, "Facebook --> ")
+if(choice == 4):
+	print("\nVous avez choisi: A votre choix")
 
-	message = post.text
+	indexLink = str(input("\nEntrez votre link ici de boursorama : "))
 
-	try:
-		server.sendmail(email_user, email_send, message)
-		print("email sent")
-	except:
-		print("error")
-	server.quit()
+	nameOfStock = input("\nEntrez le nom du stock ici : ")+" --> "
 
-	print("Success: send it !")
+	post = extracteur(driver, indexLink, element)
 
-'''
-	driver.quit()
+	emailSender(post, EMAIL_USER, EMAIL_SENDER, nameOfStock)
